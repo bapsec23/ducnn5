5be386954e0c84b39bb0cefe4bd00e66e7f05be1
bapsec23 <bapsec23@gmail.com>
2024-10-03 10:10:01 +0700

    Create up.bat

---
+++ up.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Đặt thư mục chứa các file cần upload
+set DIRECTORY_TO_UPLOAD="D:\CÔNG VIỆC"
+
+REM Địa chỉ web server Flask của bạn
+set SERVER_URL=http://10.38.18.11:9000/upload
+
+REM Duyệt qua tất cả các file trong thư mục và thư mục con
+for /r %DIRECTORY_TO_UPLOAD% %%f in (*) do (
+    echo Uploading file: "%%f"
+    curl -X POST -F "file=@%%f" %SERVER_URL%
+)
+
+echo All files have been uploaded!
